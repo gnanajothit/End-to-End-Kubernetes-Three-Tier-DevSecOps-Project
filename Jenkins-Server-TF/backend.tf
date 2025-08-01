7ad26014b3418d10667da57685ad8c6532f2aaff
gnanajothit <120785724+gnanajothit@users.noreply.github.com>
2025-08-02 00:46:24 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
